dc93b22782b9ebcba88c41a370210f3c005955e8
Taine <tcsmith2@waimea.school.nz>
2025-02-10 11:01:07 +1300

    learning some stuff

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,29 @@
-fun main() {
+/**  Kotlin intro*/
+
+/** the main function is the entry to any kotlin program */
+fun main()
+/**we can output to the terminal using print() & println()*/
+{
     println("Hello mortal!")
-    println("The end is near.")
+   println("It's time to make life balanced")
+    println("THE END IS NEAR.")
 
 }
+//strings are concatenated with a +
+    println(""+""+"")
+// in kotlin, there are two types of variables
+// if a variable will never change (immutable)
+//we use "val" (value)...
+val name = "taine"
+val dab = "26,6,2008"
+
+//if a variable will change (mutable)we use "var" (variable)
+var height = 184
+//variable names should always start with a lower case letter
+val numberoflegs = 2 //good!
+val Numberoflegs = 2 //bad!
+
+//multiple words in a variable
+val speicesofpet = "cat" //bad!
+val speciesOfPet = "cat" //Good!....CamelCase
+val species_of_pet = "cat" //Also Good!...SnakeCase
